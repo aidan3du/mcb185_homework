68a733fd04e6e6550af9e6020edbce4ec5d8e2d1
Aidan Du <aidandu@aidan3du.local>
2024-02-12 17:19:16 -0800

    Unit 4

---
+++ 44randompi.py
@@ -0,0 +1,15 @@
+# Authors: Aidan, Mo
+
+import random
+import math
+
+inside = 0
+total = 0
+while True:
+		x = random.random()
+		y = random.random()
+		distance = math.sqrt(x*x + y*y)
+		if distance < 1:
+			inside += 1
+		total += 1
+		print(inside / total * 4)
